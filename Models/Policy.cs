933a58cede387588dc853c3bec6b4cc6212d0d40
JuanApbGroup21 <juanguarecuco@apbgroup.net>
2025-09-03 11:02:43 -0400

    Sprint5 fix11 jd (#7)
    * commit corregido
    
    * confirmacion
    
    * cambios stored procedure
    
    * INGRESOS DE LOS SCRIPTS
    
    * CONFIRMACION DE CAMBIOS ANTES DE LA FUSION
    
    * modificacion de respuestas
    
    * CAMBIOS STORED PROCEDURES
    
    * MODIFICACION DE POLIZA

--- Models/Policy.cs
+++ Models/Policy.cs
@@ -28,7 +28,26 @@ namespace Models
         [Required] public Int32? PayMethodId { get; set; }
         [SwaggerIgnore][Range(1, 22)] public Int32? EstatusId { get; set; }
         [SwaggerIgnore] public String? EstatusName { get; set; }
-  
+        [SwaggerIgnore] public String? Vin { get; set; }
+        [SwaggerIgnore] public String? Plate { get; set; }
+        [SwaggerIgnore] public String? EngineSerial { get; set; }
+        [SwaggerIgnore] public String? ModelName { get; set; }
+        [SwaggerIgnore] public String? FirstName { get; set; }
+        [SwaggerIgnore] public String? LastName { get; set; }
+        [SwaggerIgnore] public String? Phone { get; set; }
+        [SwaggerIgnore] public String? Email { get; set; }
+        [SwaggerIgnore] public String? DealerName { get; set; }
+        [SwaggerIgnore] public String? DealerCod { get; set; }
+        [SwaggerIgnore] public String? SupplierCod { get; set; }
+        [SwaggerIgnore] public String? SupplierName { get; set; }
+        [SwaggerIgnore] public String? Description { get; set; }
+        [SwaggerIgnore] public String? Year { get; set; }
+        [SwaggerIgnore] public String? Color { get; set; }
+        [SwaggerIgnore] public String? Direction { get; set; }
+        [SwaggerIgnore] public String? BrandName { get; set; }
+        [SwaggerIgnore] public Int32? BrandId { get; set; }
+        [SwaggerIgnore] public Int32? SupplierId { get; set; }
+
 
     }
 }
