faf3cae5cb9c55a39b3a27528af495750b024d47
Sarahy Chirinos <132629977+sarahybcg@users.noreply.github.com>
2025-10-01 11:24:45 -0400

    Sprint6 fix8 sc (#42)
    * feat: packagelist
    
    * feat: packageslist

---
+++ Models/PackageList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Annotations;
+using Util;
+
+namespace Models
+{
+    public class PackageList
+    {
+        [SwaggerIgnore]
+        public Int32? Id { get; set; }
+
+        [SwaggerIgnore]
+        public string? Code { get; set; }
+
+        [SwaggerIgnore]
+        public Int32? SupplierId { get; set; }
+
+        [SwaggerIgnore]
+        public Int32? CustomerId { get; set; }
+
+        [SwaggerIgnore]
+        public string? SupplierName { get; set; }
+
+        [SwaggerIgnore]
+        public string? CustomerName { get; set; }   
+
+        [SwaggerIgnore]
+        public string? PartInnerCode { get; set; }
+
+        [SwaggerIgnore]
+        public string? PartName { get; set; }
+
+
+        [SwaggerIgnore]
+        public Int32? Quantity { get; set; }
+
+    }
+}
+ 
