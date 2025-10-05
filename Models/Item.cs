03a46eafe8de9754730ffb9df29f53c825217d0c
JuanApbGroup21 <juanguarecuco@apbgroup.net>
2025-10-05 07:36:18 -0400

    Sprint6 fix17 jd (#51)
    * commit corregido
    
    * confirmacion
    
    * cambios stored procedure
    
    * INGRESOS DE LOS SCRIPTS
    
    * CONFIRMACION DE CAMBIOS ANTES DE LA FUSION
    
    * modificacion de respuestas
    
    * CAMBIOS STORED PROCEDURES
    
    * MODIFICACION DE POLIZA
    
    * MODIFICAION DE VEHICLE
    
    * ARREGLO DE IMPRESION Y EXPORTACION
    
    * MODIFICACION DE import de vehiculos
    
    * cambios en autenticacion
    
    * modificacion de brand
    
    * agg stored procedures , cambios en msj
    
    * cambios
    
    * modificacion de supplierid
    
    * commit
    
    * modificacion de msj de seguridad y cambios en sp
    
    * Encabezado de seguridad
    
    * MODIFICACION DE MSJ SP
    
    * MODIFICACION DE SP PARA VALIDACIONES DE EXCEL
    
    * CAMBIOS DE IMPORTACION
    
    * MODIFICACION DE IMPORTACION PARA OBTENER PLANTA
    
    * modificacion import y exportacion
    
    * modificacion de importacion de zonas
    
    * MODIFICACION EN MSJ
    
    * MODIFICACION DE EXPORTACION
    
    * modificacion de import
    
    * ARREGLO DE LAS IMPORTACIONES
    
    * MODIFICACION DE CAMPOS F O M
    
    * MODIFACION DE SP VEHICULOS Y CONTACTOS
    
    * modificacion sp
    
    * lote de ticket de servicios y polizas
    
    * cambios y creacion de vehiculos facturados
    
    * modificacion de maintenance para recibir idsupplier
    
    * cambio de numero de orden
    
    * modificacion de maintenance
    
    * cambios de numero de orden
    
    * cambios en los campos requeridos de servicios
    
    * cambio el campor reuquerido mantenimiento
    
    * metodos para mostrar possiblefault y assistancetype
    
    * CAMBIOS DE SP
    
    * CORRECION DE EXPORTACION EXCEL
    
    * ACOMODAR DATOS DE ASISTENCIA TECNICA
    
    * cambios de reporte de falla
    
    * CAMBIO DE PARA SERVICE ASSISTANCE
    
    * modificacion de exportacion de report fail
    
    * cambios en servicios
    
    * cambios en exportacion srg
    
    * revision de importacion de parts
    
    * modficacion
    
    * encabezados de seeguridad
    
    * mas seguridad
    
    * IDUSER EN ZONAS
    
    * cambios attachment
    
    * MODIFICACION DEL PROCESAR
    
    * modificacion de controler de vehiculos full
    
    * commit de control de cambios
    
    * ARREGLO DE ATTACHMENT
    
    * SUPPLIERID PARA POLICYBY
    
    * endpoint de usuarios asignados
    
    * creacion de lista de reporte de fallas validados
    
    * configuracion de licencia
    
    * Eliminar licencias
    
    * vEHICLEID
    
    * arreglos en delete attachment
    
    * DEVOLVER FECHA DE CREACION DE ARCHIVOS
    
    * modelo y usp de elementos externos
    
    * creacion de items
    
    * cambio de respuesta en attachment

---
+++ Models/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Swashbuckle.AspNetCore.Annotations;
+
+
+namespace Models
+{
+    public class Item : Record
+    {
+
+       [Required] public String? Description { get; set; }
+       [Required] public String? Type { get; set; }
+       [Required] public Int32? SupplierId { get; set; }
+
+    }
+}
