7c1707502e16313d94635394eb7af4faca40dacd
itapb <wilmancamacho@apbgroup.net>
2025-10-09 08:21:55 -0400

    Update dInventory.cs

--- Data/dInventory.cs
+++ Data/dInventory.cs
@@ -2671,6 +2671,55 @@ namespace Data
             return _response;
         }
 
+
+
+         public async Task<Response<Result>> ImportInvoiceReport(List<Models.InvoiceReport> _list,  Int32 userId, Int32 supplierId)
+ {
+     await _semaphore.WaitAsync(Util.Setting.TimeOut);
+     try
+     {
+         return await _importInvoiceReport(_list, userId, supplierId);
+     }
+     finally
+     {
+         _semaphore.Release();
+     }
+ }
+
+ private async Task<Response<Result>> _importInvoiceReport(List<Models.InvoiceReport> _list, Int32 userId, Int32 supplierId)
+ {
+     Response<Result> _response = new Response<Result>();
+     try
+     {
+
+         string _jsonstring = Util.Json.ConvertToJsonString(_list);
+  
+
+         Util.Parameter _parameter = new Util.Parameter();
+         _parameter.AddSqlParameter("@DATA", _jsonstring);
+         _parameter.AddSqlParameter("@IDUSER", userId);
+         _parameter.AddSqlParameter("@IDSUPPLIER", supplierId);
+     
+
+         Mapping _mapping = new Mapping();
+         _mapping.SetDefaultPostMapping();
+
+         Util.Data _data = Util.Data.GetInstance();
+         DataTable _table = await _data.GetDataTable("USP_POST_INVOICEREPORT", _parameter);
+         _response.Data = _data.GetItem<Models.Result>(_mapping, _table);
+         _response.SetPostResponse();
+
+     }
+     catch (Exception ex)
+     {
+         _response.SetError(ex);
+     }
+
+     return _response;
+ }
+
+        
+
         #endregion
 
     }
