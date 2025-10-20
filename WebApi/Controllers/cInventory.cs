5909d5b2bf3d9d28b384f15878c23c3c1b278cab
Sarahy Chirinos <sarahybcg@gmail.com>
2025-10-20 13:06:46 -0400

    claim

--- WebApi/Controllers/cInventory.cs
+++ WebApi/Controllers/cInventory.cs
@@ -972,7 +972,20 @@ namespace WebApi.Controllers
 
         }
 
+        [HttpPost("/api/Guide/Claim")]
+        public async Task<IActionResult> postClaim(Int32 guideId, Int32 userId)
+        {
+            try
+            {
 
+                var _response = await _dInventory.PostClaim(guideId, userId);
+                return StatusCode(_response.Status, _response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+            }
+        }
         private MemoryStream ConvertToExcel(List<Models.Inventory> _inventories)
         {
             // 2. Crear el libro de trabajo Excel
