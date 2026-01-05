b5b17b83ad2f0d53b0a0fbf67a350d74b12c5657
Sarahy Chirinos <sarahybcg@gmail.com>
2026-01-05 13:41:50 -0400

    claim

---
+++ Models/ClaimDetails.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Models
+{
+    public class ClaimDetails 
+    {
+        [Required]
+        public int? Id { get; set; }
+        [Required]
+        public int? ClaimId { get; set; }
+
+        [Required]
+        public int? PartId { get; set; }
+
+        [Required]
+        public int? Quantity { get; set; } 
+
+        [Required]
+        public int? ReplacementPartId { get; set; }
+
+        [Required]
+        public int? ReasonId { get; set; }
+
+        [SwaggerIgnore]
+        public string? PartInnerCode { get; set; }
+
+        [SwaggerIgnore]
+        public string? PartName { get; set; }
+
+        [SwaggerIgnore]
+        public string? RemplacemetInnerCode { get; set; }
+
+        [SwaggerIgnore]  
+        public string? ReplacemetName { get; set; } 
+
+        [SwaggerIgnore]
+        public string? ReasonDescription { get; set; }
+
+        [SwaggerIgnore]
+        public string? StatusName { get; set; }
+    }
+
+}
+ 
---
+++ Models/ClaimPart.cs
@@ -0,0 +1,52 @@
+﻿using Models;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Util;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Models
+{
+    public class ClaimPart 
+    {
+        [Required]
+        public int? Id { get; set; }  
+        [Required]
+        public int? SupplierId { get; set; } 
+        [Required]
+        public int? DealerId { get; set; } 
+        [Required]
+        public int? UserId { get; set; }
+
+        public string? Reference { get; set; }  
+        public string? Note { get; set; } 
+        public string? Comment { get; set; } 
+        public string? Invoice { get; set; } 
+        public string? Guide { get; set; }
+
+        [SwaggerIgnore]
+        public string? DealerName { get; set; }  
+        [SwaggerIgnore]
+        public string? SupplierName { get; set; }  
+        [SwaggerIgnore]
+        public string? StatusName { get; set; } 
+        [SwaggerIgnore]
+        public string? FiscalName { get; set; } 
+        [SwaggerIgnore]
+        public string? TotalItems { get; set; }
+        [SwaggerIgnore]
+        public string? ApprovedItems { get; set; }
+        [SwaggerIgnore]
+        public string? RejectedItems { get; set; }
+        [SwaggerIgnore]
+        public string? PendingItems { get; set; }
+        [SwaggerIgnore]
+        public string? StatusSummary { get; set; }
+
+    }
+
+} 
