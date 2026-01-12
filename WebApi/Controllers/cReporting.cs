bf549262ab8982e9b08f65dbe00adf116a069e5e
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-01-12 09:08:16 -0400

    cambios de reporteria

--- WebApi/Controllers/cReporting.cs
+++ WebApi/Controllers/cReporting.cs
@@ -1,4 +1,5 @@
-﻿using Data;
+﻿using ClosedXML.Excel;
+using Data;
 using DocumentFormat.OpenXml.EMMA;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
@@ -44,9 +45,7 @@ namespace WebApi.Controllers
         {
             try
             {
-                var response = await _dReporting.GetAllJson(
-                    filter, rowFrom, userId, supplierId, dealerId, fromDate, upToDate, reportingId
-                );
+                var response = await _dReporting.GetAllJson(filter, rowFrom, userId, supplierId, dealerId, fromDate, upToDate, reportingId);
 
                 return StatusCode(StatusCodes.Status200OK, response);
             }
@@ -55,6 +54,111 @@ namespace WebApi.Controllers
                 return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
             }
         }
+
+
+        private MemoryStream ConvertToExcel(List<Dictionary<string, object>> data)
+        {
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("DATA");
+
+                if (data.Any())
+                {
+                    // Encabezados dinámicos
+                    var headers = data.First().Keys.ToList();
+                    for (int i = 0; i < headers.Count; i++)
+                    {
+                        worksheet.Cell(1, i + 1).Value = headers[i];
+                    }
+
+                    var headerRange = worksheet.Range(1, 1, 1, headers.Count);
+                    headerRange.Style.Fill.BackgroundColor = XLColor.LightGray;
+                    headerRange.Style.Font.Bold = true;
+                    headerRange.SetAutoFilter();
+
+                    // Filas dinámicas
+                    int row = 2;
+                    foreach (var dict in data)
+                    {
+                        for (int col = 0; col < headers.Count; col++)
+                        {
+                            var cell = worksheet.Cell(row, col + 1);
+                            var value = dict[headers[col]];
+
+                            if (value == null)
+                            {
+                                cell.Value = string.Empty;
+                            }
+                            else if (value is DateTime dt)
+                            {
+                                cell.Value = dt;
+                            }
+                            else if (value is int i)
+                            {
+                                cell.Value = i;
+                            }
+                            else if (value is decimal d)
+                            {
+                                cell.Value = d;
+                            }
+                            else if (value is bool b)
+                            {
+                                cell.Value = b ? "SI" : "NO";
+                            }
+                            else
+                            {
+                                cell.Value = value.ToString();
+                            }
+                        }
+                        row++;
+                    }
+
+
+
+                    worksheet.Columns().AdjustToContents();
+                }
+
+                var stream = new MemoryStream();
+                workbook.SaveAs(stream);
+                stream.Position = 0;
+                return stream;
+            }
+        }
+
+
+        [HttpGet("Export")]
+        public async Task<IActionResult> GetExport(string? filter, int userId, int? supplierId, int? dealerId, DateTime? fromDate, DateTime? upToDate, int reportingId)
+
+        {
+
+            try
+            {
+
+                var response = await _dReporting.GetAllJson(filter, null, userId, supplierId, dealerId, fromDate, upToDate, reportingId);
+
+                // Convertir la respuesta a una lista dinámica
+                List<Dictionary<string, object>> genericList = response.Data;
+
+                // Pasar la lista genérica al método ConvertToExcel
+                MemoryStream _excel = ConvertToExcel(genericList);
+
+
+                string _fileName = "Reporte.xlsx";
+
+                return File(
+                 _excel,
+                 "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                 _fileName);
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+
+        }
+
+        
 
 
     }
