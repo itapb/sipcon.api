45ddcd24fc25fe54f3fbb428bdaa484504c1499a
Wilman Camacho <wilmancamacho@apbgroup.net>
2026-04-06 09:38:23 -0400

    bankAccount

--- Data/dPayment.cs
+++ Data/dPayment.cs
@@ -1,5 +1,6 @@
-﻿using System.Data;
+﻿using DocumentFormat.OpenXml.Spreadsheet;
 using Models;
+using System.Data;
 using Util;
 
 
@@ -154,6 +155,34 @@ namespace Data
             }
             return _response;
         }
+        private async Task<Response<List<Models.BankAccount>>> _getBankAccounts(Int32 supplierId)
+        {
+            Response<List<Models.BankAccount>> _response = new Response<List<Models.BankAccount>>();
+
+            try
+            {
+
+                Util.Parameter _parameter = new Util.Parameter();
+                _parameter.AddSqlParameter("@IDSUPPLIER", supplierId);
+
+                Mapping _mapping = new Mapping();
+                _mapping.AddItem("Id", "ID");
+                _mapping.AddItem("Name", "VNAME");
+                _mapping.AddItem("Code", "VCODE");
+                _mapping.AddItem("Account", "VACCOUNT");
+
+                Util.Data _data = Util.Data.GetInstance();
+                DataTable _table = await _data.GetDataTable("USP_GET_BANKACCOUNTS", _parameter);
+                _response.Data = _data.GetList<Models.BankAccount>(_mapping, _table);
+                _response.SetGetResponse(_table);
+
+            }
+            catch (Exception ex)
+            {
+                _response.SetError(ex);
+            }
+            return _response;
+        }
 
         public async Task<Response<List<Models.Currency>>> GetCurrencys()
         {
@@ -215,6 +244,17 @@ namespace Data
                 _semaphore.Release();
             }
         }
-    
+        public async Task<Response<List<Models.BankAccount>>> GetBankAccounts(Int32 supplierId)
+        {
+            await _semaphore.WaitAsync(Util.Setting.TimeOut);
+            try
+            {
+                return await _getBankAccounts(supplierId);
+            }
+            finally
+            {
+                _semaphore.Release();
+            }
+        }
     }
 }
