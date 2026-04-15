4ecee76b2cab0bac3d0ada3a8caa91a9f68e1a50
Jose Diaz <josediaz@apbgroup.net>
2026-04-15 10:16:50 -0400

    añadiendo los modelos, controladores y datas de las inspecciones

---
+++ Models/Inspection.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Models
+{
+    public class Inspection : Record
+    {
+        [Required] public int? CreatedBy { get; set; }
+        [Required] public int? VehicleId { get; set; }
+        [Required] public int? AreaId { get; set; }
+
+        public int? InitBy { get; set; }
+        public int? ClosedBy { get; set; }
+        public int? TransporterId { get; set; }
+        public int? RecepBy { get; set; }
+
+        [SwaggerIgnore] public String? UserName { get; set; }
+        [SwaggerIgnore] public String? InitByName { get; set; }
+        [SwaggerIgnore] public String? ClosedByName { get; set; }
+        [SwaggerIgnore] public String? TransporterName { get; set; }
+        [SwaggerIgnore] public String? RecepByName { get; set; }
+
+        [SwaggerIgnore] public String? VehiclePlate { get; set; }
+        [SwaggerIgnore] public String? Model { get; set; }
+        [SwaggerIgnore] public String? Lote { get; set; }
+        [SwaggerIgnore] public String? Vin { get; set; }
+        [SwaggerIgnore] public String? NameArea { get; set; }
+        [SwaggerIgnore] public DateTime? Created { get; set; }
+
+        public DateTime? DInit { get; set; }
+        public DateTime? DClose { get; set; }
+        public DateTime? DReception { get; set; }
+
+        [SwaggerIgnore] public Int32? IsCompleted { get; set; }
+    }
+
+    public class InspectionFase : Record
+    {
+        [Required] public Int32? InspectionId { get; set; }
+        [Required] public Int32? FaseId { get; set; }
+        [SwaggerIgnore] public String? Fase { get; set; }
+        [SwaggerIgnore] public DateTime? CompletedDate { get; set; }
+        [SwaggerIgnore] public Int32? IsCompleted { get; set; }
+
+        [SwaggerIgnore] public Int32? AreaId { get; set; }
+        [SwaggerIgnore] public String? Area { get; set; }
+    }
+
+    public class FullInspection
+    {
+        [Required] public Int32? VehicleId { get; set; }
+        [Required] public Int32? AreaId { get; set; }
+    }
+
+    public class InspectionDetail : Record
+    {
+        [Required] public Int32? Value { get; set; }
+        [Required] public String? Observation { get; set; }
+        [SwaggerIgnore] public String? FileUrl { get; set; }
+        [Required] public Int32? InspectionId { get; set; }
+        [Required] public Int32? FeatureId { get; set; }
+        [SwaggerIgnore] public String? Feature { get; set; }
+        [SwaggerIgnore] public Int32? FeatureTypeId { get; set; }
+        [SwaggerIgnore] public String? FeatureType { get; set; }
+        [SwaggerIgnore] public String? FaseId { get; set; }
+        [SwaggerIgnore] public String? Fase { get; set; }
+        [SwaggerIgnore] public Int32? AreaId { get; set; }
+        [SwaggerIgnore] public String? Area { get; set; }
+        [SwaggerIgnore] public String? Color { get; set; }
+        [SwaggerIgnore] public String? Model { get; set; }
+        [SwaggerIgnore] public String? Vin { get; set; }
+        [SwaggerIgnore] public String? Plate { get; set; }
+    }
+}
