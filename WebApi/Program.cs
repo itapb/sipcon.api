bc7a32cb8687dba149cc99f781acda367e8c8c53
Jose Diaz <josediaz@apbgroup.net>
2026-04-17 11:02:56 -0400

    Fix puerto localhost

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -97,8 +97,6 @@ builder.Services.AddScoped<dReporting>();
 builder.Services.AddScoped<dPayment>();
 builder.Services.AddScoped<dInspection>();
 
-builder.WebHost.UseUrls("http://*:5294");
-// 5. crear app
 var app = builder.Build();
 
 
