5bc45bac26e202a2c770853ec17b18cd1b4a9577
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-04-23 14:54:48 -0400

    creacion de la tasa por servicio

--- Models/Rate.cs
+++ Models/Rate.cs
@@ -1,12 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
-using Swashbuckle.AspNetCore.Annotations;
+﻿using Swashbuckle.AspNetCore.Annotations;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace Models
 {
     public class Rate : Record
     {
+        [JsonPropertyName("fechaActualizacion")]
         public DateTime DDate { get; set; }
+
+        [JsonPropertyName("promedio")]
         public decimal NRate { get; set; }
 
     }
