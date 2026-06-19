0db1344c08168ebbc39678bd383da9f09662a256
Juan Guarecuco <juanguarecuco@apbgroup.net>
2026-06-19 11:57:00 -0400

    cambios de reporteria figo

--- Models/Figo.cs
+++ Models/Figo.cs
@@ -24,27 +24,12 @@ namespace Models
         public int ReportFigoId { get; set; }
     }
 
-    public class FIGO_ReportCxC
+
+    public class FIGO_Query
     {
-        public long Id { get; set; }
-        public string? DealerName { get; set; }
-        public string? Vat { get; set; }
-        public string? Client { get; set; }
-        public string? Phone { get; set; }
-        public string? Zone { get; set; }
-        public string? Occurrence { get; set; }
-        public string? Document { get; set; }
-        public string? IssueDate { get; set; }
-        public string? DueDate { get; set; }
-        public int OverdueDays { get; set; }
-        public string? Currency { get; set; }
-        public string? DocumentCurrency { get; set; }
-        public decimal OverdueAmount { get; set; }
-        public decimal CurrentAmount { get; set; }
-        public decimal TotalDebt { get; set; }
-        public decimal ExchangeRate { get; set; }
-        public string? Product { get; set; }
-        public string? SerialNumber { get; set; }
-        public string? ProductGroup { get; set; }
+        public int Id { get; set; }
+        public string? Query { get; set; }
     }
+
+
 }
--- WebApi/Controllers/cFigo.cs
+++ WebApi/Controllers/cFigo.cs
@@ -47,81 +47,99 @@ namespace WebApi.Controllers
             }
         }
 
+        [HttpGet("GetReportsContent")]
+        public async Task<IActionResult> GetAllJson(int userId, int reportId, string jsonParameters)
+        {
+            try
+            {
+                var response = await _dFigo.GetAllJson(userId, reportId, jsonParameters);
+
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+
+
+
         #endregion "Reports"
 
         #region "CxC"
 
-        [HttpGet("ReportCxC")]
-        public async Task<IActionResult> GetReportCxC(DateTime? Date, string Currency, string? filter)
-        {
-            try
-            {
-                DateTime FinalDate = Date ?? DateTime.Today;
-                var _response = await _dFigo.GetReportCxC(FinalDate, Currency, filter);
-                return StatusCode(_response.Status, _response);
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
-            }
-        }
+        //[HttpGet("ReportCxC")]
+        //public async Task<IActionResult> GetReportCxC(DateTime? Date, string Currency, string? filter)
+        //{
+        //    try
+        //    {
+        //        DateTime FinalDate = Date ?? DateTime.Today;
+        //        var _response = await _dFigo.GetReportCxC(FinalDate, Currency, filter);
+        //        return StatusCode(_response.Status, _response);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+        //    }
+        //}
 
-        [HttpGet("ExportReportCxC")]
-        public async Task<IActionResult> ExportReportCxC(DateTime? Date, string Currency, string? filter)
-        {
-            try
-            {
-                DateTime FinalDate = Date ?? DateTime.Today;
-                var _response = await _dFigo.GetReportCxC(FinalDate, Currency, filter);
+        //[HttpGet("ExportReportCxC")]
+        //public async Task<IActionResult> ExportReportCxC(DateTime? Date, string Currency, string? filter)
+        //{
+        //    try
+        //    {
+        //        DateTime FinalDate = Date ?? DateTime.Today;
+        //        var _response = await _dFigo.GetReportCxC(FinalDate, Currency, filter);
 
-                if (_response.Data == null)
-                {
-                    return StatusCode(_response.Status, _response);
-                }
+        //        if (_response.Data == null)
+        //        {
+        //            return StatusCode(_response.Status, _response);
+        //        }
 
-                MemoryStream _excel = ExportExcel.ConvertToExcelReportCxC(_response.Data, FinalDate);
-                string _fileName = $"ReporteCxC_{FinalDate:yyyyMMdd}.xlsx";
+        //        MemoryStream _excel = ExportExcel.ConvertToExcelReportCxC(_response.Data, FinalDate);
+        //        string _fileName = $"ReporteCxC_{FinalDate:yyyyMMdd}.xlsx";
 
-                return File(
-                    _excel,
-                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-                    _fileName
-                );
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
-            }
-        }
+        //        return File(
+        //            _excel,
+        //            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+        //            _fileName
+        //        );
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+        //    }
+        //}
 
-        [HttpGet("ExportReportCxCPdf")]
-        public async Task<IActionResult> ExportReportCxCPdf(DateTime? Date, string Currency, string? filter)
-        {
-            try
-            {
-                DateTime FinalDate = Date ?? DateTime.Today;
+        //[HttpGet("ExportReportCxCPdf")]
+        //public async Task<IActionResult> ExportReportCxCPdf(DateTime? Date, string Currency, string? filter)
+        //{
+        //    try
+        //    {
+        //        DateTime FinalDate = Date ?? DateTime.Today;
 
-                var _response = await _dFigo.GetReportCxC(FinalDate, Currency, filter);
+        //        var _response = await _dFigo.GetReportCxC(FinalDate, Currency, filter);
 
-                if (_response.Data == null)
-                {
-                    return StatusCode(_response.Status, _response);
-                }
+        //        if (_response.Data == null)
+        //        {
+        //            return StatusCode(_response.Status, _response);
+        //        }
 
-                byte[] _pdfBytes = ExportPDF.ConvertToPdfReportCxC(_response.Data, FinalDate);
-                string _fileName = $"ReporteCxC_{FinalDate:yyyyMMdd}.pdf";
+        //        byte[] _pdfBytes = ExportPDF.ConvertToPdfReportCxC(_response.Data, FinalDate);
+        //        string _fileName = $"ReporteCxC_{FinalDate:yyyyMMdd}.pdf";
 
-                return File(
-                    _pdfBytes,
-                    "application/pdf",
-                    _fileName
-                );
-            }
-            catch (Exception ex)
-            {
-                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
-            }
-        }
+        //        return File(
+        //            _pdfBytes,
+        //            "application/pdf",
+        //            _fileName
+        //        );
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+        //    }
+        //}
         #endregion "CxC"
     }
 }
