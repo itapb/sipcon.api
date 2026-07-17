da95c492fa8b36a9945abcc23b3696f5236274c1
Jose Diaz <josediaz@apbgroup.net>
2026-07-17 11:18:50 -0400

    API para la intregración de POWERBI

---
+++ Models/PowerBI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Models
+{
+    public class PowerBI_Reports : Record   
+    {
+        [SwaggerIgnore] public string? Report { get; set; }
+        [SwaggerIgnore] public string? ReportId { get; set; }
+        [SwaggerIgnore] public string? WorkSpace { get; set; }
+        [SwaggerIgnore] public string? WorkSpaceId { get; set; }
+    }
+
+    public class PowerBI_Token
+    {
+        [SwaggerIgnore] public string? Token { get; set; }
+    }
+
+    public class ResponseToken
+    {
+       [SwaggerIgnore] public string? access_token { get; set; }
+    }
+}
---
+++ WebApi/Controllers/cPowerBI.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Data;
+using Models;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace WebApi.Controllers
+{
+
+    [Route("api/PowerBI")]
+    [ApiController]
+    [Authorize]
+
+    public class cPowerBI : ControllerBase
+    {
+
+        private readonly dPowerBI _dPowerBI;
+
+        public cPowerBI(dPowerBI dPowerBI)
+        {
+            _dPowerBI = dPowerBI;
+        }
+
+        [HttpGet("GetReports")]
+        public async Task<IActionResult> GetReports(string userId)
+        {
+            try
+            {
+                var _response = await _dPowerBI.GetReports(userId);
+                return StatusCode(_response.Status, _response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+            }
+        }
+
+        [HttpGet("GetToken")]
+        public async Task<IActionResult> GetTokenApi()
+        {
+            try
+            {
+                var _response = await _dPowerBI.GetTokenAPI();
+                return StatusCode(_response.Status, _response);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status409Conflict, ex.Message);
+            }
+        }
+    }
+}
